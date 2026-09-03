6b0b98a6f5112cf2017a81509d130b308475e3d0
Akande peter <akandepeter86@gmail.com>
2026-09-03 09:25:28 +0100

    feat: add CopyPageButton to documentation pages

--- app/(docs)/[[...slug]]/page.tsx
+++ app/(docs)/[[...slug]]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from 'next/navigation';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import { useMDXComponents } from '@/mdx-components';
 import { PageFeedback } from '@/components/mdx/PageFeedback';
+import { CopyPageButton } from '@/components/mdx/CopyPageButton';
 
 export default async function Page(props: {
   params: Promise<{ slug?: string[] }>;
@@ -27,9 +28,14 @@ export default async function Page(props: {
         single: false,
       }}
     >
-      <DocsTitle className="zorveus-page-title">
-        {page.data.title}
-      </DocsTitle>
+      <div className="flex items-start justify-between gap-4">
+        <DocsTitle className="zorveus-page-title">
+          {page.data.title}
+        </DocsTitle>
+        <div className="pt-2 shrink-0">
+          <CopyPageButton slug={params.slug} />
+        </div>
+      </div>
       {page.data.description && (
         <DocsDescription className="text-zinc-400 text-sm sm:text-base leading-relaxed mt-2">
           {page.data.description}
